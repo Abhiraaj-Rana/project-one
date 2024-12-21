cbb4c6daa1709194020fcb802f3250bb576b4d01
Abhiraaj Rana <abhiraajrana@outlook.com>
2024-12-21 22:10:23 +0530

    I am learning Git

--- one.py
+++ one.py
@@ -1,1 +1,6 @@
 print("Python journey begins")
+print("Hi my name is Abhiraaj, I am starting my Python and Git journey")
+print("For adding my files to Git tracking I am using git add . command")
+print("For commiting my file changes to Git I am using git commit -m '''commit message''' command")
+print("To make sure no other person has updated the branch I am using git pull origin <branch name> command to fetch updated changes if any")
+print("For pusing my commited chnages to Git I am using git push origin <branch name> command")
